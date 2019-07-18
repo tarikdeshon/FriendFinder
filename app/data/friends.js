5a4402acadd94d59a5eddf5e5a261453aba5a587
Tarik P <tarikdeshon@gmail.com>
2019-07-18 14:44:42 -0700

    create.file.&js

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [{
+        name: "Penny Hardaway",
+        photo: "https://cdn.vox-cdn.com/thumbor/5dCYV_3xGTXRSGO2vnVh6z_YWu8=/130x266:2062x1560/1200x800/filters:focal(715x628:1043x956)/cdn.vox-cdn.com/uploads/chorus_image/image/62855660/1397388.jpg.0.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Tmac",
+        photo: "http://www2.pictures.gi.stylebistro.com/Tracy+McGrady+Athletic+Shoes+Basketball+Sneakers+UlDT1na6-11l.jpg",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Shaq",
+        photo: "https://i.pinimg.com/originals/85/2c/21/852c218ce983b4233807a0c6c865b375.jpg",
+        scores: [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Lebron ",
+        photo: "https://www.discoverlosangeles.com/sites/default/files/styles/hero/public/media/sports/lebron_james_preseason_2018.jpg?itok=-l1GeKAX",
+        scores: [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Kobe",
+        photo: "https://usatftw.files.wordpress.com/2014/06/usp-nba_-los-angeles-lakers-at-sacramento-kings.jpg?w=1000&h=600&crop=1",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "MJ",
+        photo: "https://imagesvc.timeincapp.com/v3/fan/image?url=https%3A%2F%2Fpippenainteasy.com%2Fwp-content%2Fuploads%2Fgetty-images%2F2017%2F10%2F521181842-chicago-bulls-v-portland-trailblazers.jpg.jpg&w=850&h=560&c=sc",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
